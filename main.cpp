acb81f0b0d585e2abc12c990615381c951c19015
abbott221 <abbottm07@gmail.com>
2014-03-25 17:21:35 -0400

    update preset movements

--- main.cpp
+++ main.cpp
@@ -137,13 +137,15 @@ int main(void)
     //put the movement presets here???
 
 
-    moreMoves[0].setMovement(8, 120);
+        moreMoves[0].setMovement(8, 120);
         moreMoves[1].setMovement(50, 1.50);
-        moreMoves[2].setMovement(6, 4.698);
-        moreMoves[3].setMovement(4, 16.2);
-        moreMoves[4].setMovement(5, 5.00);
-        moreMoves[5].setMovement(12, 2.00);
-        moreMoves[6].setMovement(10, 7.0);
+        moreMoves[2].setMovement(6, 4.697);
+        moreMoves[3].setMovement(4, 21.2);
+        moreMoves[4].setMovement(5, 4.00);
+        //moreMoves[5].setMovement(12, 2.00);
+        moreMoves[6].setMovement(21, 5.0);
+        moreMoves[7].setMovement(8, 45);
+        moreMoves[8].setMovement(8, 120);
 
 
         moreMoves[12].setMovement(6, 4.0);
